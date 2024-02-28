4ca397d09172e2a2480089b3a9a18187f2484eb9
Sridhar-Konidela <sridharkonidela@outlook.com>
2024-02-28 12:02:17 +0530

    Fixed Header click

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -56,7 +56,7 @@ export default function Header() {
 
 
     <header>
-    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 sticky top-0">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <Link href="/" class="flex items-center">
                 <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
@@ -72,21 +72,23 @@ export default function Header() {
                 </button> */}
             </div>
             <div class=" justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                <ul class="flex flex-row mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <ul class="flex flex-row mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 flex-wrap">
                     <li>
-                        <Link href="/services" class={`"  block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white ${Pathname==='/' && '!text-blue-600 border-blue-600'}"`} aria-current="page">Services</Link>
+                        <Link href="/services" class={`"  block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-gray-400 ${Pathname==='/services' && '!text-white border-white'} "`} aria-current="page">Services</Link>
+                    </li>
+                   
+                    <li>
+                        <Link href="/security" class={`" block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-gray-400 ${Pathname==='/security' && '!text-white border-white'}  "`}>Security</Link>
+                    </li>
+                    
+                    <li>
+                        <Link href="/team" class={`" block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-gray-400 ${Pathname==='/team' && '!text-white border-white'}  "`}>Team</Link>
                     </li>
                     <li>
-                        <Link href="/security" class={`"block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700 ${Pathname==='/' && '!text-blue-600 border-blue-600'}"`}>Security</Link>
+                        <Link href="/insights" class={`" block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-gray-400 ${Pathname==='/insights' && '!text-white border-white'}  "`}>Insights</Link>
                     </li>
                     <li>
-                        <Link href="/team" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Team</Link>
-                    </li>
-                    <li>
-                        <Link href="/insights" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Insights</Link>
-                    </li>
-                    <li>
-                        <Link href="/contact" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</Link>
+                        <Link href="/contact" class={`" block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-gray-400 ${Pathname==='/contact' && '!text-white border-white'} "`}>Contact</Link>
                     </li>
                 </ul>
               </div>
