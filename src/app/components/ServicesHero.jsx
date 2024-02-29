045554cbdd76e6b75964778b12706e94c38ab4f8
Sridhar-Konidela <sridharkonidela@outlook.com>
2024-02-29 11:13:24 +0530

    Added service Hero component to service page

---
+++ src/app/components/ServicesHero.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function ServicesHero({title,desc,bg}) {
+  return (
+    <div className={`bg-${bg}`}>
+        <div className='w-[80%] flex flex-col items-center justify-center mx-auto py-7 gap-3 '>
+            <h1 className=' font-serif font-bold text-3xl'>{title}</h1>
+            <p className='text-center text-xl'>{desc}</p>
+            <Link href='/contact' className='pt-5'>
+                <button className='flex text-white bg-violet-500 py-3 px-7 rounded-full' >REQUEST A CALL</button>
+            </Link>
+        </div>
+    </div>
+  )
+}
--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import SearchHero from '../components/SearchHero'
 import Benefits from '../components/Benefits'
+import ServicesHero from '../components/ServicesHero'
 
 export default function Service() {
   return (
@@ -9,6 +10,16 @@ export default function Service() {
       <SearchHero heading={'Medical coding automation services built for you'}
                 matter={"DHANVANTRI's AI seamlessly intergrates into your workflow"}
                 button={'GET STARTED'}
+      />
+      <ServicesHero
+        title={'STANDARDIZING DATA'}
+        desc={'standardizing data involves the process of ensuring that all numerical information used for analysis, billing, or research purposes follows a uniform scale or measurement system. This helps facilitate accurate comparisons, analysis, and decision-making across different healthcare records and databases.'}
+        bg={'white'}
+      />
+      <ServicesHero
+        title={'Production coding'}
+        desc={'production coding is the development of software solutions that are ready for deployment and use by end-users or customers. It often follows a series of development processes, including coding, testing, quality assurance, and deployment, to ensure that the software meets performance, reliability, and functionality requirements in real-world scenarios.'}
+        bg={'gray-200'}
       />
       <Benefits
         heading1={"Faster, cheaper, and more accurate"}
