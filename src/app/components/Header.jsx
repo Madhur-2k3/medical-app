db8b081fea4ea3a10ad31c7e7c96a8c6546d95bb
Sridhar-Konidela <sridharkonidela@outlook.com>
2024-03-02 17:51:47 +0530

    Modified logo

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,6 +3,7 @@
 import Link from 'next/link'
 import { useRouter,usePathname } from 'next/navigation'
 import React from 'react'
+import './Header.css'
 // import About from './About'
 
 export default function Header() {
@@ -56,11 +57,11 @@ export default function Header() {
 
 
     <header className='sticky top-0 z-10'>
-    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 sticky top-0">
+    <nav class=" border-gray-200 px-4 lg:px-6 py-2.5 bg-gray-800 sticky top-0">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <Link href="/" class="flex items-center">
                 <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">DHANVANTRI</span>
+                <span class="self-center text-xl font-semibold whitespace-nowrap logo">DHANVANTRI</span>
             </Link>
             <div class="flex items-center lg:order-2">
                 {/* <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
