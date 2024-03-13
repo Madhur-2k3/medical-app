bc91710c5b43a6a3e12358ba1ef3e286695dce99
Sridhar-Konidela <sridharkonidela@outlook.com>
2024-03-13 09:22:53 +0530

    Added bg to SearchHero Component

--- src/app/components/SearchHero.jsx
+++ src/app/components/SearchHero.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
+import './SearchHero.css'
 
 export default function SearchHero({heading,matter,button,buttonType,location}) {
   return (
-    <div className='flex space-x-1 flex-col items-center text-center mx-auto gap-10 py-10 text-white bg-violet-400 w-full h-fit'>
+    <div className=' first-hero flex space-x-1 flex-col items-center text-center mx-auto gap-10 py-10 text-white bg-violet-400 w-full h-fit'>
       <h1 className=' text-3xl sm:text-5xl  font-sans font-medium'>{heading}</h1>
       <p className=' text-xl sm:text-2xl font-sans font-normal sm:font-semibold'>{matter}</p>
       <Link href='/contact'>
